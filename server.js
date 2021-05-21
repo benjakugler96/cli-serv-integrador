478a1ee1bf2654d76a5476bc1c47e6ae3ee32a84
benjamin.kugler <benjamin.kugler@elliemae.com>
2021-05-21 11:33:02 -0300

    fix: removed morgan for deployment

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ require('dotenv').config({ path: './config/config.env' });
 const app = express();
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
-}
-
 // Security middleware
 app.use(cors());
 
